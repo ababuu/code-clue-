a4f42dbffb0a3a70e45652b19c9bb5f203d407fd
ababu <ababualemu123@gmail.com>
2025-12-18 19:43:18 +0300

    feat: add client boilerplate code

---
+++ client/src/actions/index.js
@@ -0,0 +1,37 @@
+"use server";
+
+export async function explain(prevState, formData) {
+  const codeSnippet = formData.get("code");
+  const language = formData.get("language");
+
+  try {
+    const response = await fetch(
+      `${import.meta.env.VITE_API_BASE_URL}/explain-code`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ codeSnippet, language }),
+      }
+    );
+
+    if (!response.ok) {
+      const errorData = await response.json().catch(() => ({}));
+      return {
+        success: false,
+        error: errorData.error || "Failed to fetch explanation from server.",
+      };
+    }
+    const data = await response.json();
+    return {
+      success: true,
+      data,
+    };
+  } catch (error) {
+    return {
+      success: false,
+      error: ` An error occurred: ${error.message} `,
+    };
+  }
+}
